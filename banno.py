d58e8462d9d7ea528ff5959136973d695d1a797d
orcutt989 <orcutt989@gmail.com>
2020-01-08 03:31:30 -0500

    asynchronous processing and basic stat start

--- banno.py
+++ banno.py
@@ -1,12 +1,32 @@
 import os
 import requests
 import json
+import time
+import sys
+import asyncio
+from reprint import output
+import datetime as dt
 from pprint import pprint
 from requests.auth import AuthBase
 from requests.auth import HTTPBasicAuth
 
 consumer_key = os.environ['KEY']
 consumer_secret = os.environ['SECRET']
+
+log_interval=5
+start_time = time.time()
+
+stats = {
+  "total_tweets": 0,
+  "avg_per_sec": 0,
+  "avg_per_min": 0,
+  "avg_per_hr": 0,
+  "perc_emoji": 0,
+  "top_hashtags": [],
+  "perc_url": 0,
+  "perc_img": 0,
+  "top_domains": []
+  }
 
 stream_url = "https://api.twitter.com/labs/1/tweets/stream/sample"
 
@@ -35,14 +55,30 @@ class BearerTokenAuth(AuthBase):
     r.headers['Authorization'] = f"Bearer %s" % self.bearer_token
     return r
 
-def stream_connect(auth):
+async def stream_connect(auth):
   response = requests.get(stream_url, auth=auth, headers={"User-Agent": "TwitterDevSampledStreamQuickStartPython"}, stream=True)
   for response_line in response.iter_lines():
     if response_line:
-      pprint(json.loads(response_line))
+      # global stats
+      # stats["total_tweets"]+=1
+      await process_tweet(json.loads(response_line))
+
+async def process_tweet(tweet):
+  global stats, start_time
+  stats["total_tweets"]+=1
+  time_elapsed = int(time.time() - start_time)
+
+  if time_elapsed >= log_interval:
+    log_to_console(stats)
+    start_time = time.time()
+
+def log_to_console(stats):
+  os.system('clear')
+  pprint(stats)
+  
 
 bearer_token = BearerTokenAuth(consumer_key, consumer_secret)
 
 # Listen to the stream. This reconnection logic will attempt to reconnect as soon as a disconnection is detected.
 while True:
-  stream_connect(bearer_token)
+  asyncio.run(stream_connect(bearer_token))
